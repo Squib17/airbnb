2da16245e5d098d967314a84a6121be978c5fe3d
Saquib Hasnain <saquib.hasnain@kellogg.northwestern.edu>
2024-01-16 02:53:37 +0000

    Practice Exercise - SQL

--- practice-4.sql
+++ practice-4.sql
@@ -1,5 +1,6 @@
 -- How many listings are in Lincoln Park?
-
+SELECT COUNT(neighborhood) FROM listings
+WHERE neighborhood == "Lincoln Park";
 -- +----------+
 -- | 272      |
 -- +----------+
--- practice-6.sql
+++ practice-6.sql
@@ -1,5 +1,8 @@
 -- How many reviews are written per neighborhood?
-
+SELECT listings.neighborhood, COUNT(reviews.listing_id)
+FROM listings
+INNER JOIN reviews ON reviews.listing_id = listings.id
+GROUP BY listings.neighborhood;
 -- +------------------------+----------+
 -- | Albany Park            | 2557     |
 -- | Archer Heights         | 541      |
